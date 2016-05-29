58f980aac6675b932ec13b16e63181e6800f9f01
David Lonjon <david.lonjon@gmail.com>
2016-05-29 22:09:01 +0900

    Fix namespace issue

--- src/YonderWeb/AppNexus/AppNexusArray.php
+++ src/YonderWeb/AppNexus/AppNexusArray.php
@@ -14,7 +14,7 @@ namespace YonderWeb\AppNexus;
  * @author Moiz Merchant <moiz@exactdrive.com>
  * @version $Id$
  */
-class AppNexusArray implements ArrayAccess, IteratorAggregate, Countable
+class AppNexusArray implements \ArrayAccess, \IteratorAggregate, \Countable
 {
 
     //-------------------------------------------------------------------------
