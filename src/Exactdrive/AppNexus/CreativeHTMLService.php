87cc1b6b8305cec195a1030cee1f1c5750a2d5fa
Nikola Stojiljković <nikola.stojiljkovic@tamedia.ch>
2019-05-31 15:58:08 +0200

    Add full list of fields. Needs to be filtered to exclude fields which can not be sent through the API.

--- src/Exactdrive/AppNexus/CreativeHTMLService.php
+++ src/Exactdrive/AppNexus/CreativeHTMLService.php
@@ -16,7 +16,69 @@ class CreativeHTMLService extends Api
     const TARGET_ADVERTISER = 'target_advertiser';
     const TARGET_PUBLISHER = 'target_publisher';
 
-    public static $fields = [];
+    /**
+     * Creative properties which can be updated with AppNexus server.
+     *   https://wiki.appnexus.com/pages/viewpage.action?title=Creative+HTML+Service&spaceKey=api#CreativeHTMLService-JSONFields
+     *
+     * @var array
+     */
+    public static $fields = [
+        'adservers',
+        'advertiser_id',
+        'adx_audit',
+        'allow_audit',
+        'allow_safety_pacing',
+        'allow_ssl_audit',
+        'audit_feedback',
+        'audit_status',
+        'brand',
+        'brand_id',
+        'campaigns',
+        'categories',
+        'click_track_result',
+        'click_url',
+        'code',
+        'code2',
+        'competitive_brands',
+        'competitive_categories',
+        'created_on',
+        'currency',
+        'custom_macros',
+        'daily_budget',
+        'daily_budget_imps',
+        'enable_pacing',
+        'file_name',
+        'first_run',
+        'folder',
+        'id',
+        'is_control',
+        'is_expired',
+        'is_hosted',
+        'is_prohibited',
+        'is_self_audited',
+        'language',
+        'last_modified',
+        'last_run',
+        'lifetime_budget',
+        'lifetime_budget_imps',
+        'line_items',
+        'media_assets',
+        'media_url',
+        'media_url_secure',
+        'member_id',
+        'name',
+        'profile_id',
+        'publisher_id',
+        'segments',
+        'sla',
+        'sla_eta',
+        'ssl_status',
+        'state',
+        'status',
+        'technical_attributes',
+        'template',
+        'type',
+    ];
 
     /**
      * @throws Exception
