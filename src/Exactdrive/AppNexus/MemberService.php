6eb5a9df4097b0de06d8a06961a627544094b016
Nikola Stojiljković <nikola.stojiljkovic@tamedia.ch>
2019-06-05 10:02:08 +0200

    Add member service - get member.

---
+++ src/Exactdrive/AppNexus/MemberService.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Exactdrive\AppNexus;
+
+use Exception;
+
+/**
+ * Class MemberService
+ *
+ * @package Exactdrive\AppNexus
+ */
+class MemberService extends Api
+{
+    /**
+     * @throws Exception
+     *
+     * @return string
+     */
+    public static function getBaseUrl(): string
+    {
+        $url = Api::getBaseUrl() . '/member';
+
+        return $url;
+    }
+
+    /**
+     * @throws Exception
+     *
+     * @return AppNexusObject
+     */
+    public static function getMemberId(): AppNexusObject
+    {
+        $url = self::getBaseUrl();
+        $response = self::makeRequest($url, Api::GET);
+
+        return new AppNexusObject($response, AppNexusObject::MODE_READ_WRITE);
+    }
+}
